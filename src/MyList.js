75b7454d56aafa4f4073768dbb03caa44442da1d
mark.cherepovskyi <mark.cherepovskyi@gmail.com>
2023-03-17 00:14:04 +0200

    v1.1

--- src/MyList.js
+++ src/MyList.js
@@ -4,5 +4,4 @@ function MyList(props) {
     const items = props.items.map(item => <li>{item}</li>);
     return <div className={"my-list"}><ul className={"my-list__list"}>{items}</ul></div>
 }
-
 export default MyList;
